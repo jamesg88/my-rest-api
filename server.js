2a5db5809694f6c0954e1d0e5a60325dacef9d5b
James <james.gordon1@verizon.com>
2021-11-02 15:47:10 -0500

    Updates

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //server.js houses all my server code 
 
+require('dotenv').config()
 const express =  require('express')
 const app = express() 
 const mongoose = require('mongoose')
 
 //connect to database 
-mongoose.connect('mongodb://localhost/fans', {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
